0837747a1d08eaba5602f3deab0645002128c644
liulq <1125052258@qq.com>
2021-06-27 19:21:09 +0800

    second commit

--- DormitoryManagerWeb/src/com/ischoolbar/programmer/config/BaseConfig.java
+++ DormitoryManagerWeb/src/com/ischoolbar/programmer/config/BaseConfig.java
@@ -7,7 +7,8 @@ package com.ischoolbar.programmer.config;
 public class BaseConfig {
 	public static int ADMIN_STATUS_ENABLE = 1;
 	public static int ADMIN_STATUS_DISAABLE = 0;
-	public static String dbUrl = "jdbc:mysql://localhost:3306/db_dormitory_manager_web?useUnicode=true&characterEncoding=utf8";
-	public static String dbUser = "root";
-	public static String dbPassword = "123456dsc";
+	public static String dbUrl = "jdbc:mysql://121.36.219.177:3306/db_dormitory_manager_web?useUnicode=true&characterEncoding=utf8";
+	public static String dbUser = "houkun";
+	public static String dbPassword = "Dba123456";
+	public static String jdbcName = "com.mysql.jdbc.Driver";
 }
